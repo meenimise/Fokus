e57ad3a4a5a858a15760f8bdfa48f96fe8d90864
Nguyen Tri Minh <minh.ngntri@gmail.com>
2022-06-15 17:45:03 +0700

    Fix Home page layout

--- pages/index.js
+++ pages/index.js
@@ -111,7 +111,7 @@ function Home() {
                 <Header headerText={'Welcome back, ' + removeVI(session?.user.name, { ignoreCase: false, replaceSpecialCharacters: false }) + '! 👋'}>
                 </Header>
 
-                <div className='relative mt-[30px] mx-auto w-[90%] h-[510px]'>
+                <div className='relative mt-[30px] mx-auto w-[90%] h-[410px]'>
                     <div className='absolute w-full h-[96px] drop-shadow-[0_10px_60px_rgba(226,236,249,1)] bg-white rounded-[15px]'>
                         <div className='relative mt-[20px] container mx-auto w-[92%] h-[56px]'>
                             <div className='absolute w-[30%] h-full hover:cursor-pointer'>
@@ -164,11 +164,11 @@ function Home() {
                         </div>
                     </div>
 
-                    <div className='absolute mt-[111px] w-full h-[192px]'>
-                        <div className='absolute w-[49%] h-full drop-shadow-[0_10px_60px_rgba(226,236,249,1)] bg-white rounded-[15px] hover:cursor-pointer hover:bg-purple_light'
+                    <div className='absolute mt-[111px] w-full h-[142px]'>
+                        <div className='absolute w-[49%] h-full grid items-center justify-center drop-shadow-[0_10px_60px_rgba(226,236,249,1)] bg-white rounded-[15px] hover:cursor-pointer hover:bg-purple_light'
                         onClick={() => setShowPopUpCreateSession(true)}
                         >
-                            <div className='relative mt-[30px] container mx-auto w-[300px] h-[132px]'>
+                            <div className='relative w-[300px] h-[120px]'>
                                 <div className='absolute container grid place-items-center font-poppins font-semibold text-black text-[12pt] whitespace-nowrap truncate select-none ...'>
                                     Create a new Session
                                 </div>
@@ -177,14 +177,14 @@ function Home() {
                                     Be a powerful Session Administrator
                                 </div>
 
-                                <div className='absolute ml-[110px] bottom-0 h-[80px] w-[80px]' style={{color: theme.extend.colors.purple}}>
+                                <div className='absolute ml-[110px] bottom-0 h-[70px] w-[70px]' style={{color: theme.extend.colors.purple}}>
                                     <PlusCircleIcon></PlusCircleIcon>
                                 </div>
                             </div>
                         </div>
 
-                        <div className='absolute right-0 w-[49%] h-full drop-shadow-[0_10px_60px_rgba(226,236,249,1)] bg-white rounded-[15px] hover:cursor-pointer hover:bg-purple_light'>
-                            <div className='relative mt-[30px] container mx-auto w-[300px] h-[132px]'>
+                        <div className='absolute right-0 w-[49%] h-full grid items-center justify-center drop-shadow-[0_10px_60px_rgba(226,236,249,1)] bg-white rounded-[15px] hover:cursor-pointer hover:bg-purple_light'>
+                            <div className='relative w-[300px] h-[120px]'>
                                 <div className='absolute container grid place-items-center font-poppins font-semibold text-black text-[12pt] whitespace-nowrap truncate select-none ...'>
                                     Join an existing Session
                                 </div>
@@ -193,33 +193,29 @@ function Home() {
                                     Stay "fokused" with other members
                                 </div>
 
-                                <div className='absolute ml-[110px] bottom-0 h-[80px] w-[80px]' style={{color: theme.extend.colors.purple}}>
+                                <div className='absolute ml-[110px] bottom-0 h-[70px] w-[70px]' style={{color: theme.extend.colors.purple}}>
                                     <ViewGridAddIcon></ViewGridAddIcon>
                                 </div>
                             </div>
                         </div>                     
                     </div>
 
-                    <div className='absolute mt-[318px] w-full h-[192px] drop-shadow-[0_10px_60px_rgba(226,236,249,1)] bg-white rounded-[15px]'>
-                        <div className='relative mt-[32px] w-[90%] h-[128px] container mx-auto'>
+                    <div className='absolute mt-[268px] w-full h-[142px] drop-shadow-[0_10px_60px_rgba(226,236,249,1)] bg-white rounded-[15px]'>
+                        <div className='relative mt-[7px] w-[90%] h-[128px] container mx-auto'>
                             <div className='absolute h-full w-[49%]'>
                                 <Logo className='w-full h-full scale-[60%]'></Logo>
                             </div>
 
-                            <div className='absolute right-0 h-full w-[49%] font-poppins text-purple text-sm select-none'>
+                            <div className='absolute right-0 h-full grid items-center justify-center w-[49%] font-poppins text-purple text-sm select-none'>
                                 "Create a Session or join one with your friends via the specific ID, you’ll never find avoiding your mobile phones while dealing with tasks a big trouble. Stay 'FOKUSED' now or never."
-                            </div>
-
-                            <div className='absolute right-0 bottom-0 h-[15%] w-[49%] font-poppins text-grey text-sm text-right select-none'>
-                                The Fokus Team
-                            </div>            
+                            </div>         
                         </div>
                     </div>
                      
                     {
                     showPopUpCreateSession ==  true ?
                     <div className='flex items-center justify-center h-full w-full'>
-                        <div className='w-[60%] h-[85%] drop-shadow-[0_10px_60px_rgba(226,236,249,1)] bg-white rounded-[15px]'>
+                        <div className='w-[60%] h-[90%] drop-shadow-[0_10px_60px_rgba(226,236,249,1)] bg-white rounded-[15px]'>
                             <div className='h-[15%] w-full flex items-center justify-center font-poppins text-xm font-medium select-none'>
                                 Create Session
 
