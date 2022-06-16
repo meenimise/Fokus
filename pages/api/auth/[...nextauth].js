29c3c7dcea4101ddb0f70de9b8b2ac1822bccfe4
Nguyen Tri Minh <minh.ngntri@gmail.com>
2022-06-16 16:59:21 +0700

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -24,20 +24,20 @@ export default NextAuth({
     }),
     secret: process.env.SECRET,
 
-    // callbacks: {
-    //     async jwt({ token, user, account, profile, isNewUser }) {
-    //         user && (token.user = user)
-    //         return token
-    //     },
-    //     async session({ session, token, user }) {
-    //         session = {
-    //             ...session,
-    //             user: {
-    //                 id: user.id,
-    //                 ...session.user
-    //             }
-    //         }
-    //         return session
-    //     }
-    // }
+    callbacks: {
+        async jwt({ token, user, account, profile, isNewUser }) {
+            user && (token.user = user)
+            return token
+        },
+        async session({ session, token, user }) {
+            session = {
+                ...session,
+                user: {
+                    id: user.id,
+                    ...session.user
+                }
+            }
+            return session
+        }
+    }
 });
