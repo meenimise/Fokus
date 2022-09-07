4585288ee0196a55d9db2638dc1e490fccecf1ac
Nguyen Tri Minh <minh.ngntri@gmail.com>
2022-09-07 14:47:57 +0700

    Add rewritten functions

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,4 +1,4 @@
-import { FirebaseAdapter } from "@next-auth/firebase-adapter";
+import { FirebaseAdapter } from "../../../rewritten/@next-auth/firebase-adapter";
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 import { db } from "../../../firebase/firebase.config";
