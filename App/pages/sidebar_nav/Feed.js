1b7fd5f506790135be701408db6f32f4aeacfa98
Nguyễn Trí Minh <minh.ngntri@gmail.com>
2022-12-02 09:22:47 +0700

    Update Feed.js

--- App/pages/sidebar_nav/Feed.js
+++ App/pages/sidebar_nav/Feed.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import { useSession } from 'next-auth/react';
+import Home from '..';
 
 function Feed() {
-  return (
-    <div>Feed</div>
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return(
+        <div></div>
+    )
+  }
+
+  if (status === "authenticated") {``
+    return (
+      <Sidebar>
+        <div className="flex h-full flex-col justify-center items-center">
+          <h1 className="text-4xl mb-5 font-bold">{"Fokus Feed"}</h1>
+        </div>
+      </Sidebar>
+    )
+  }
+
+  return(
+    <Home></Home>
   )
 }
 
